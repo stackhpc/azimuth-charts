a2a7b1d4fde65aff9135d1f6259e68773b36fb7d
Scott Davidson <scott@stackhpc.com>
2023-05-30 09:39:13 +0100

    Be explicit about defaulting to docker.io

--- skopeo-manifests/create-skopeo-manifest.py
+++ skopeo-manifests/create-skopeo-manifest.py
@@ -24,19 +24,41 @@ if len(sys.argv) != 2:
 
 file_path = Path(sys.argv[1])
 # Kubernetes assumes docker registry by default:
-# https://kubernetes.io/docs/concepts/containers/images/#image-names
-default_registry = "docker.io"
+DEFAULT_REGISTRY = "docker.io"
+KNOWN_REGISTRIES = [
+    "docker.io",
+    "ghcr.io",
+    "gcr.io",
+    "quay.io",
+    "registry.k8s.io",
+    "k8s.gcr.io",
+]
 
 
 def split_image_url(url: str):
+    """
+    Split image url into constituent parts.
+
+    The logic here is intended to handle urls where the registry is the content
+    of the string up to the first '/' and also cases where the registry is
+    not explicitly include in the string, meaning that docker.io should be
+    used since it is the default k8s registry:
+    https://kubernetes.io/docs/concepts/containers/images/#image-names
+    """
     try:
         parts = url.strip("\n").split("/")
-        registry = parts[0] if len(parts) > 1 else default_registry
+        registry = parts[0] if len(parts) > 1 else DEFAULT_REGISTRY
+        # Handle case where registry image path had a '/' in it but
+        # image registry wasn't included in path
+        if registry not in KNOWN_REGISTRIES:
+            parts[1] = registry + "/" + parts[1]
+            registry = DEFAULT_REGISTRY
         # Don't copy images that are already in ghcr
         if registry == "ghcr.io":
             return {}
         repo_plus_version = "/".join(parts[1:]) if len(parts) > 1 else parts[0]
         repo, version = repo_plus_version.split(":")
+        # Handle case where sha is include in image url
         if "@sha256" in repo:
             repo = repo.replace("@sha256", "")
             version = "sha256:" + version
